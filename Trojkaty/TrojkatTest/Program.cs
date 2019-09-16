c16cf6c1aa66be41b85634fb99b57ac553872dbb
Artuqu <percikartur@gmail.com>
2019-09-16 20:28:50 +0200

    wer.2

--- Trojkaty/TrojkatTest/Program.cs
+++ Trojkaty/TrojkatTest/Program.cs
@@ -11,10 +11,17 @@ namespace TrojkatTest
         static void Main(string[] args)
         {
             Trojkat x;
-            x = new Trojkat(2, 1, 3.2);
+            x = new Trojkat(2, 1, 2.5);
             Console.WriteLine(x); //bez zawartości drukuje tylko jakiego typu jest x czyli TrojkatTest
             Console.WriteLine(x.Obwod() );//funkcja Obwod
             Console.WriteLine(x.Obwód);// properties get Obwód
+            // x.C = 4; // przypisanie 4 do c
+            Console.WriteLine(x.Obwód);
+            Console.WriteLine(x.Pole); //NaN rozwiązanie to nie jest liczba not a number
+
+
+            var y = new Trojkat();
+            Console.WriteLine(y);
         }
         
       
--- Trojkaty/TrojkatTest/Trojkat.cs
+++ Trojkaty/TrojkatTest/Trojkat.cs
@@ -10,17 +10,64 @@ namespace TrojkatTest
     {
         // fields
         private decimal a;
+        public double A
+        {
+            get { return (double)a; }
+            set {
+                if (!jestWarunekTrojkata((decimal)value, b, c))
+                    throw new ArgumentException("Nie można zmienić boku A");
+
+                    a = (decimal)value; } 
+                }
+
+
         private decimal b;
+        public double B
+        {
+            get => (double)b; // inn sposób zapisu tego samego
+            set {
+                if (!jestWarunekTrojkata(a, (decimal)value, c))
+                    throw new ArgumentException("Nie można zmienić boku B");
+
+                b = (decimal)value;
+            }
+           
+        }
+
+
         private decimal c;
+        public double C
+        { get => (double)c;
+            set {
+                if (!jestWarunekTrojkata(a, b, (decimal)value))
+                    throw new ArgumentException("Nie można zmienić boku C");
+
+                c = (decimal)value; }
+        }
+        private bool jestWarunekTrojkata(decimal x, decimal y, decimal z)
+        {
+            return x + y > z && x + z > y && z + y > x;
+        }
 
         // konstruktory
         public Trojkat(double a, double b, double c)
         {
+            if (a <= 0 || b <= 0 || c <= 0) // || znak mówiący lub
+                throw new ArgumentException("Argument musi być dodatni.");
+
+            if (!jestWarunekTrojkata ((decimal)a, (decimal)b, (decimal)c) )
+                throw new ArgumentException("Nie spełniono warunku trójkąta.");
+
+
             this.a = (decimal)a; // this odwołuje się do pola fields
             this.b = (decimal)b;
             this.c = (decimal)c;
         }
 
+        public Trojkat()
+        {
+            a = b = c = 1;
+        }
         // przeciążenie ToString
         public override string ToString()
         {
@@ -35,7 +82,24 @@ namespace TrojkatTest
         public double Obwód
         {
             get { return (double)(a + b + c); }
+        }
 
+        public double Pole
+        {
+            get //pobiera wartość
+            {
+                decimal p = (decimal)(0.5 * Obwód);
+                return Math.Sqrt( (double)(p * (p - a) * (p - b) * (p - c)) ); // sqrt to pierwiastek
+            }
+        }
+
+       public bool jestProsotkatny
+        {
+            get
+            {
+               // if () ;
+                throw new NotImplementedException();
+            }
         }
     }
 }
