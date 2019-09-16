25a373f7ad2b9f820f6d0f3bf50b2b44c998818b
Artuqu <percikartur@gmail.com>
2019-09-16 18:46:31 +0200

    wer.1

---
+++ Trojkaty/TrojkatTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrojkatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat x;
+            x = new Trojkat(2, 1, 3.2);
+            Console.WriteLine(x); //bez zawartości drukuje tylko jakiego typu jest x czyli TrojkatTest
+            Console.WriteLine(x.Obwod() );//funkcja Obwod
+            Console.WriteLine(x.Obwód);// properties get Obwód
+        }
+        
+      
+    }
+}
